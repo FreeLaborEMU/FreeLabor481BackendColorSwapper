a07b1406e9c89754d8444d5f490cd19593cc70ec
danthomps1999 <danthomps1999@gmail.com>
2023-03-02 13:42:12 -0500

    Uploading default Spring Project
    Able to import restController now, so it should be okay to move forward
    
    Issue#N/A

---
+++ FreeLabor481BackendColorSwapper/src/main/java/com/example/demo/FreeLabor481BackendColorSwapperApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeLabor481BackendColorSwapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreeLabor481BackendColorSwapperApplication.class, args);
+	}
+
+}
