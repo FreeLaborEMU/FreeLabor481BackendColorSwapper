07697dd02795216f88ee0638bee90335474f407e
Desh776 <tshmayda@emich.edu>
2023-04-05 02:55:15 -0400

    experimenting

--- FreeLabor481BackendColorSwapper/src/main/java/com/example/demo/FreeLabor481BackendColorSwapperApplication.java
+++ FreeLabor481BackendColorSwapper/src/main/java/com/example/demo/FreeLabor481BackendColorSwapperApplication.java
@@ -1,22 +1,26 @@
 package com.example.demo;
 
 import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.*;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
 import java.io.*;
 
 
 import java.io.FileInputStream;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 @SpringBootApplication
 public class FreeLabor481BackendColorSwapperApplication {
 
 	public static void main(String[] args) throws IOException {
-		// Use a service account
+		// Use a service account	localhost:8080/colorPalette/upload
 		InputStream serviceAccount = new FileInputStream("./colorswapper-firebase.json");
 		GoogleCredentials credentials = GoogleCredentials.fromStream(serviceAccount);
 		FirebaseOptions options = new FirebaseOptions.Builder()
@@ -25,16 +29,30 @@ public class FreeLabor481BackendColorSwapperApplication {
 
 		boolean hasApp = false;
 		List<FirebaseApp> firebaseApps = FirebaseApp.getApps();
-		for(FirebaseApp app : firebaseApps){
+		for(FirebaseApp app : firebaseApps) {
 			if(app.getName().equals(FirebaseApp.DEFAULT_APP_NAME)){
 				hasApp=true;
 				break;
 			}
 		}
-		if(!hasApp){
+		if(!hasApp) {
 			FirebaseApp.initializeApp(options);
 		}
 		SpringApplication.run(FreeLabor481BackendColorSwapperApplication.class, args);
+
+		fileCollector moe = new fileCollector(credentials);
+		Resource resource = new ClassPathResource("files/All_Paint_Mixes.dat");
+		InputStream upload= resource.getInputStream();
+		try {
+			String doe = moe.Download();
+			System.out.print(doe+"   ok");
+			moe.Upload(upload);
+		} catch (ExecutionException e) {
+			throw new RuntimeException(e);
+		} catch (InterruptedException e) {
+			throw new RuntimeException(e);
+		}
+
 	}
 
 }
---
+++ FreeLabor481BackendColorSwapper/src/main/java/com/example/demo/fileCollector.java
@@ -0,0 +1,100 @@
+package com.example.demo;
+
+import com.google.api.core.ApiFuture;
+
+import com.google.auth.oauth2.AccessToken;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.auth.oauth2.IdTokenCredentials;
+import com.google.auth.oauth2.IdTokenProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.cloud.StorageClient;
+import com.google.cloud.firestore.CollectionReference;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.DocumentSnapshot;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.storage.*;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.cloud.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
+public class fileCollector {
+    private String Name;
+    private GoogleCredentials cred;
+    public fileCollector(GoogleCredentials credentials) {
+        cred=credentials;
+    }
+
+
+    @GetMapping(path = "/download")
+    public String Download() throws ExecutionException, InterruptedException {
+        String firestorage;
+        Firestore db = FirestoreClient.getFirestore();
+        Iterable<DocumentReference> has =db.collection("users").listDocuments();
+        ApiFuture<DocumentSnapshot> future;
+        DocumentSnapshot document;
+
+        for(DocumentReference docstore: has)
+        {
+
+
+            future=docstore.get();
+            document= future.get();
+            firestorage =document.getData().values().toString();
+            String[] blob=firestorage.split(",");
+            blob[0]=blob[0].substring(1,blob[0].length());
+            blob [1]=blob[1].trim();
+            blob[1]=blob[1].substring(0,blob[1].length());
+            blob[2]=blob[2].substring(0,blob[2].length()-1);
+            blob [2]=blob[2].trim();
+
+            if(blob[0].equals("false"))
+            {
+                System.out.println(blob[1] + "(" + blob[2]);
+                Name=blob[2];
+                return blob[1];
+
+            }
+
+
+        }
+
+        Name="done";
+        return "done";
+
+    }
+
+
+
+    @GetMapping(path = "/download")
+    public String DownloadV2() throws ExecutionException, InterruptedException {
+        Storage storage = StorageOptions.newBuilder().setCredentials(cred).build().getService();
+        BlobId blobId= BlobId.of("colorswapper-f6b50.appspot.com","files/");
+        return storage.get(blobId).toString();
+
+
+    }
+
+
+    @GetMapping(path = "/upload")
+    public void Upload(InputStream upload) throws ExecutionException, InterruptedException, IOException {
+        Storage storage = StorageOptions.newBuilder().setCredentials(cred).build().getService();
+
+
+        BlobId blobId=BlobId.of("colorswapper-f6b50.appspot.com","files/"+Name+"/All_Paint_Mixes");
+        BlobInfo blobInfo= BlobInfo.newBuilder(blobId).setContentType("application/octet-stream").build();
+        Blob uploading =storage.create(blobInfo,upload);
+
+
+    }
+}
