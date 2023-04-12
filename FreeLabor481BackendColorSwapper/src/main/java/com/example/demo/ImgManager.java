52fd49dc11e39b487bff7687dac6b03cb3322062
Daniel Thompson <90717001+danthomps1999@users.noreply.github.com>
2023-04-12 15:31:22 -0400

    Pushing changes to keep executable code

--- FreeLabor481BackendColorSwapper/src/main/java/com/example/demo/ImgManager.java
+++ FreeLabor481BackendColorSwapper/src/main/java/com/example/demo/ImgManager.java
@@ -138,7 +138,7 @@ public class ImgManager {
 		// System.out.println(img.getHeight());
 
 		for (int i = 0; i < img.getHeight(); i++) {
-			for (int j = 0; j < img.getWidth(); j++) {
+			for (int j = i + 1; j < img.getWidth(); j++) {
 				// get RGB value of pixle as 1 value
 				int RGBValue = img.getRGB(j, i);
 				// put it into a color object which then has methods to seperate the color
